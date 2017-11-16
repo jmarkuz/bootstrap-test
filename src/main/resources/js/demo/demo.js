5c1c3df612c9172a74ec228579522a8d627a51a1
mkuziv <MKuziv@luxoft.com>
2017-11-16 19:44:51 +0200

    #'jsplumb' add anchors on all nodes manually //TODO: need dinamic add logic

--- src/main/resources/js/demo/demo.js
+++ src/main/resources/js/demo/demo.js
@@ -1,10 +1,28 @@
 ;
 (function () {
+        // left tree nodes
+        var node_11 = document.getElementById("ch11_anchor");
+        var node_12 = document.getElementById("ch12_anchor");
+        var node_13 = document.getElementById("ch13_anchor");
+        var node_14 = document.getElementById("ch14_anchor");
+        var node_15 = document.getElementById("ch15_anchor");
+        var node_16 = document.getElementById("ch16_anchor");
+        var node_17 = document.getElementById("ch17_anchor");
+        var node_18 = document.getElementById("ch18_anchor");
 
-        var b1 = document.getElementById("ch11__anchor");
-        var b2 = document.getElementById("ch21__anchor");
+        // right tree nodes
+        var node_21 = document.getElementById("ch21_anchor");
+        var node_22 = document.getElementById("ch22_anchor");
+        var node_23 = document.getElementById("ch23_anchor");
+        var node_24 = document.getElementById("ch24_anchor");
+        var node_25 = document.getElementById("ch25_anchor");
+        var node_26 = document.getElementById("ch26_anchor");
+        var node_27 = document.getElementById("ch27_anchor");
+        var node_28 = document.getElementById("ch28_anchor");
 
-        var listDiv = {b1, b2},
+        // node list: node_1*-left tree, node_2*- right tree nodes
+        var listDiv = {node_11, node_12, node_13, node_14, node_15, node_16, node_17, node_18,
+                       node_21, node_22, node_23, node_24, node_25, node_26, node_27, node_28},
 
         showConnectionInfo = function (s) {
             listDiv.innerHTML = s;
@@ -142,12 +160,57 @@
                     alert("Cannot drop connection " + info.connection.id + " : maxConnections has been reached on Endpoint " + info.endpoint.id);
                 };
 
-            var e1 = instance.addEndpoint("ch11__anchor", {anchor: "RightMiddle"}, exampleEndpoint3);
             // can bind for a maxConnections callback using a standard bind call, but can also supply 'onMaxConnections' in an Endpoint definition - see exampleEndpoint3 above.
-            e1.bind("maxConnections", maxConnectionsCallback);
+            // left hand tree anchors
+            var e11 = instance.addEndpoint("ch11_anchor", {anchor: "RightMiddle"}, exampleEndpoint3);
+            e11.bind("maxConnections", maxConnectionsCallback);
 
-            var e2 = instance.addEndpoint('ch21__anchor', {anchor: "LeftMiddle"}, exampleEndpoint3);
-            e2.bind("maxConnections", maxConnectionsCallback);
+            var e12 = instance.addEndpoint("ch12_anchor", {anchor: "RightMiddle"}, exampleEndpoint3);
+            e12.bind("maxConnections", maxConnectionsCallback);
+
+            var e13 = instance.addEndpoint("ch13_anchor", {anchor: "RightMiddle"}, exampleEndpoint3);
+            e13.bind("maxConnections", maxConnectionsCallback);
+
+            var e14 = instance.addEndpoint("ch14_anchor", {anchor: "RightMiddle"}, exampleEndpoint3);
+            e14.bind("maxConnections", maxConnectionsCallback);
+
+            var e15 = instance.addEndpoint("ch15_anchor", {anchor: "RightMiddle"}, exampleEndpoint3);
+            e15.bind("maxConnections", maxConnectionsCallback);
+
+            var e16 = instance.addEndpoint("ch16_anchor", {anchor: "RightMiddle"}, exampleEndpoint3);
+            e16.bind("maxConnections", maxConnectionsCallback);
+
+            var e17 = instance.addEndpoint("ch17_anchor", {anchor: "RightMiddle"}, exampleEndpoint3);
+            e17.bind("maxConnections", maxConnectionsCallback);
+
+            var e18 = instance.addEndpoint("ch18_anchor", {anchor: "RightMiddle"}, exampleEndpoint3);
+            e18.bind("maxConnections", maxConnectionsCallback);
+
+
+            // right hand tree anchors
+            var e21 = instance.addEndpoint('ch21_anchor', {anchor: "LeftMiddle"}, exampleEndpoint3);
+            e21.bind("maxConnections", maxConnectionsCallback);
+
+            var e22 = instance.addEndpoint('ch22_anchor', {anchor: "LeftMiddle"}, exampleEndpoint3);
+            e22.bind("maxConnections", maxConnectionsCallback);
+
+            var e23 = instance.addEndpoint('ch23_anchor', {anchor: "LeftMiddle"}, exampleEndpoint3);
+            e23.bind("maxConnections", maxConnectionsCallback);
+
+            var e24 = instance.addEndpoint('ch24_anchor', {anchor: "LeftMiddle"}, exampleEndpoint3);
+            e24.bind("maxConnections", maxConnectionsCallback);
+
+            var e25 = instance.addEndpoint('ch25_anchor', {anchor: "LeftMiddle"}, exampleEndpoint3);
+            e25.bind("maxConnections", maxConnectionsCallback);
+
+            var e26 = instance.addEndpoint('ch26_anchor', {anchor: "LeftMiddle"}, exampleEndpoint3);
+            e26.bind("maxConnections", maxConnectionsCallback);
+
+            var e27 = instance.addEndpoint('ch27_anchor', {anchor: "LeftMiddle"}, exampleEndpoint3);
+            e27.bind("maxConnections", maxConnectionsCallback);
+
+            var e28 = instance.addEndpoint('ch28_anchor', {anchor: "LeftMiddle"}, exampleEndpoint3);
+            e28.bind("maxConnections", maxConnectionsCallback);
 
             // make .window divs draggable
             instance.draggable(jsPlumb.getSelector(".drag-drop-demo .window"));
